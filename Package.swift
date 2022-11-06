b14add472475472ca27bc37b4c70d37cf2495b38
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-06 07:56:21 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["PrepViews"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.22"),
-        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.209"),
+        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.108"),
+        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.215"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
---
+++ Sources/PrepViews/MacrosIndicator/MacrosIndicator.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+import PrepDataTypes
+
+public struct MacrosIndicator: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    let carb, fat, protein: Double
+    
+    public init(_ searchResult: FoodSearchResult) {
+        self.carb = searchResult.carb
+        self.fat = searchResult.fat
+        self.protein = searchResult.protein
+    }
+    
+    let width: CGFloat = 30
+    
+    public var body: some View {
+        HStack(spacing: 0) {
+            if totalEnergy == 0 {
+                Color.clear
+                    .background(Color(.quaternaryLabel).gradient)
+            } else {
+                Color.clear
+                    .frame(width: carbWidth)
+                    .background(Macro.carb.fillColor(for: colorScheme).gradient)
+                Color.clear
+                    .frame(width: fatWidth)
+                    .background(Macro.fat.fillColor(for: colorScheme).gradient)
+                Color.clear
+                    .frame(width: proteinWidth)
+                    .background(Macro.protein.fillColor(for: colorScheme).gradient)
+            }
+        }
+        .frame(width: width, height: 10)
+        .cornerRadius(2)
+        .shadow(radius: 1.5, x: 0, y: 1.5)
+    }
+    
+    var totalEnergy: CGFloat {
+        (carb * KcalsPerGramOfCarb) + (protein * KcalsPerGramOfProtein) + (fat * KcalsPerGramOfFat)
+    }
+    var carbWidth: CGFloat {
+        guard totalEnergy != 0 else { return 0 }
+        return ((carb * KcalsPerGramOfCarb) / totalEnergy) * width
+    }
+    
+    var proteinWidth: CGFloat {
+        guard totalEnergy != 0 else { return 0 }
+        return ((protein * KcalsPerGramOfProtein) / totalEnergy) * width
+    }
+    
+    var fatWidth: CGFloat {
+        guard totalEnergy != 0 else { return 0 }
+        return ((fat * KcalsPerGramOfFat) / totalEnergy) * width
+    }
+}
