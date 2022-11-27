1a9150c27a9700bb857489345fd33e9f19b00f78
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-27 19:01:06 +0500

    bump

---
+++ Sources/PrepViews/NutrientMeter/NutrientMeter2.ViewModel.swift
@@ -0,0 +1,295 @@
+import SwiftUI
+
+public extension NutrientMeter2 {
+    class ViewModel: ObservableObject {
+        
+        @Published public var component: NutrientMeterComponent
+
+        /// Used to convey that this is for a component that has been generated (either an implicit daily goal or a meal subgoal),
+        /// as we may want to style it differently
+        @Published public var isGenerated: Bool
+        
+        @Published public var goalLower: Double?
+        @Published public var goalUpper: Double?
+
+        @Published public var planned: Double
+        @Published public var eaten: Double?
+        @Published public var increment: Double?
+
+        //TODO: Remove this
+        @Published public var burned: Double
+        
+        public init(
+            component: NutrientMeterComponent,
+            isGenerated: Bool = false,
+            goalLower: Double? = nil,
+            goalUpper: Double? = nil,
+            burned: Double = 0,
+            planned: Double,
+            increment: Double
+        ) {
+            self.component = component
+            self.isGenerated = isGenerated
+            self.goalLower = goalLower
+            self.goalUpper = goalUpper
+            self.burned = burned
+            self.planned = planned
+            self.eaten = nil
+            self.increment = increment
+        }
+        
+        public init(
+            component: NutrientMeterComponent,
+            isGenerated: Bool = false,
+            goalLower: Double? = nil,
+            goalUpper: Double? = nil,
+            burned: Double = 0,
+            planned: Double,
+            eaten: Double
+        ) {
+            self.component = component
+            self.isGenerated = isGenerated
+            self.goalLower = goalLower
+            self.goalUpper = goalUpper
+            self.burned = burned
+            self.planned = planned
+            self.eaten = eaten
+            self.increment = nil
+        }
+    }
+}
+
+public extension NutrientMeter2.ViewModel {
+    var remainingString: String {
+        return "TODO"
+//        guard let goal else { return "" }
+//        return "\(Int(goal + burned - planned - (increment ?? 0)))"
+    }
+    
+    var goalString: String {
+        return "TODO"
+//        guard let goal else { return "" }
+//        return "\(Int(goal))"
+    }
+    
+    var burnedString: String {
+        "\(Int(burned))"
+    }
+    
+    var foodString: String {
+        "\(Int(planned + (increment ?? 0)))"
+    }
+    
+//    var incrementString: String {
+//        "\(Int(increment ?? 0))"
+//    }
+}
+
+extension NutrientMeter2.ViewModel: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(component)
+        hasher.combine(goalLower)
+        hasher.combine(goalUpper)
+        hasher.combine(burned)
+        hasher.combine(planned)
+        hasher.combine(eaten)
+        hasher.combine(increment)
+    }
+}
+
+extension NutrientMeter2.ViewModel: Equatable {
+    public static func ==(lhs: NutrientMeter2.ViewModel, rhs: NutrientMeter2.ViewModel) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+}
+
+public extension NutrientMeter2.ViewModel {
+    var haveGoal: Bool {
+        goalLower != nil || goalUpper != nil
+    }
+    
+    var showingIncrement: Bool {
+        increment != nil
+    }
+    
+    var highestGoal: Double? {
+        goalUpper ?? goalLower
+    }
+    
+    var totalGoal: Double {
+        /// Returned `planned` when we have no goal so that the entire meter becomes the planned amount
+        guard let highestGoal else {
+            return planned
+        }
+        return highestGoal + burned
+    }
+    
+    var goalBoundsType: GoalBoundsType {
+        if goalLower != nil {
+            if goalUpper != nil {
+                return .lowerAndUpper
+            } else {
+                return .lowerOnly
+            }
+        } else if goalUpper != nil {
+            return .upperOnly
+        } else {
+            return .none
+        }
+    }
+    
+
+    var eatenPercentageType: PercentageType {
+        guard preppedPercentageType != .excess else {
+            return .excess
+        }
+        return PercentageType(eatenPercentage)
+    }
+    
+    var eatenPercentage: Double {
+        guard let eaten = eaten, totalGoal != 0 else { return 0 }
+        //        guard let eaten = eaten?.wrappedValue, totalGoal != 0 else { return 0 }
+        if preppedPercentage < 1 {
+            return eaten / totalGoal
+        } else {
+            guard planned != 0 else { return 0 }
+            return eaten / planned
+        }
+    }
+    
+    var normalizdEatenPercentage: Double {
+        if eatenPercentage < 0 {
+            return 0
+        } else if eatenPercentage > 1 {
+            return 1.0/eatenPercentage
+        } else {
+            return eatenPercentage
+        }
+    }
+    
+    var preppedPercentageForMeter: Double {
+        /// Choose greater of preppedPercentage or prepped/(prepped + increment)
+        if let increment = increment,
+           totalGoal + increment > 0,
+           planned / (totalGoal + increment) > preppedPercentage
+        {
+            return planned / (planned + increment)
+        } else {
+            return preppedPercentage
+        }
+    }
+    
+    var preppedPercentage: Double {
+        guard totalGoal != 0 else { return 0 }
+        
+        let total: Double
+        if let increment = increment,
+           planned + increment > totalGoal
+        {
+            //        if let increment = increment?.wrappedValue,
+            //           food + increment > totalGoal
+            //        {
+            total = planned + increment
+        } else {
+            total = totalGoal
+        }
+        
+        return planned / total
+    }
+
+    var percentageType: PercentageType {
+        if let _ = increment {
+            return incrementPercentageType
+        } else {
+            return preppedPercentageType
+        }
+    }
+}
+
+public extension NutrientMeter2.ViewModel {
+    
+    var preppedColor: Color {
+        switch percentageType {
+        case .empty:
+            return Color("StatsEmptyFill", bundle: .module)
+        case .regular:
+            return component.preppedColor
+        case .complete:
+            return haveGoal ? Colors.Complete.placeholder : component.preppedColor
+        case .excess:
+            return haveGoal ? Colors.Excess.placeholder : component.preppedColor
+        }
+    }
+    
+    var incrementColor: Color {
+        switch incrementPercentageType {
+        case .empty:
+            return Color("StatsEmptyFill", bundle: .module)
+        case .regular:
+            return component.eatenColor
+        case .complete:
+            return haveGoal ? Colors.Complete.fill : component.eatenColor
+        case .excess:
+            return haveGoal ? Colors.Excess.fill : component.eatenColor
+        }
+    }
+    
+    var eatenColor: Color {
+        guard preppedPercentageType != .complete else {
+            return haveGoal ? Colors.Complete.fill : component.eatenColor
+        }
+        
+        switch eatenPercentageType {
+        case .empty:
+            return Color("StatsEmptyFill", bundle: .module)
+        case .regular:
+            return component.eatenColor
+        case .complete:
+            return haveGoal ? Colors.Complete.fill : component.eatenColor
+        case .excess:
+            return haveGoal ? Colors.Excess.fill : component.eatenColor
+        }
+    }
+}
+
+public extension NutrientMeter2.ViewModel {
+    struct Colors {
+        public struct Complete {
+            public static let placeholder = Color("StatsCompleteFillExtraNew", bundle: .module)
+            public static let fill = Color("StatsCompleteFill", bundle: .module)
+            public static let text = Color("StatsCompleteText", bundle: .module)
+            public static let textDarker = Color("StatsCompleteTextExtra", bundle: .module)
+        }
+        
+        public struct Excess {
+            public static let placeholder = Color("StatsExcessFillExtra", bundle: .module)
+            public static let fill = Color("StatsExcessFill", bundle: .module)
+            public static let text = Color("StatsExcessText", bundle: .module)
+            public static let textDarker = Color("StatsExcessTextExtra", bundle: .module)
+        }
+        
+        public struct Empty {
+            public static let fill = Color("StatsEmptyFill", bundle: .module)
+            public static let text = Color("StatsEmptyText", bundle: .module)
+            public static let textLighter = Color("StatsEmptyTextSecondary", bundle: .module)
+        }
+    }
+}
+
+extension NutrientMeter2.ViewModel {
+    
+    var labelTextColor: Color {
+        guard haveGoal else { return component.textColor }
+        switch percentageType {
+        case .empty:
+            return Colors.Empty.text
+        case .regular:
+            return component.textColor
+        case .complete:
+            return Colors.Complete.text
+        case .excess:
+            return Colors.Excess.text
+        }
+    }
+    
+}
---
+++ Sources/PrepViews/NutrientMeter/NutrientMeter2.swift
@@ -0,0 +1,340 @@
+import SwiftUI
+
+public struct NutrientMeter2: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    @ObservedObject var viewModel: NutrientMeter.ViewModel
+    
+    @State private var hasAppeared = false
+    
+    public init(viewModel: NutrientMeter.ViewModel) {
+        self.viewModel = viewModel
+    }
+}
+
+public extension NutrientMeter2 {
+    var body: some View {
+        GeometryReader { proxy -> AnyView in
+            return AnyView(
+                capsulesPrototype(proxy)
+            )
+        }
+        //TODO: Rewrite this
+//        .if(hasAppeared, transform: { view in
+//            /// Use this to make sure the animation only gets applied after the view has appeared, so that it doesn't animate it during it being transitioned into view
+//            view
+//                .animation(animation, value: eaten)
+//        })
+        .clipShape(
+            shape
+        )
+        .task {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                hasAppeared = true
+            }
+        }
+    }
+
+    var shape: some Shape {
+        RoundedRectangle(cornerRadius: 5)
+//            Capsule()
+    }
+    
+
+    func capsulesPrototype(_ proxy: GeometryProxy) -> some View {
+        
+        var placeholderCapsule: some View {
+            shape
+                .fill(placeholderColor)
+        }
+        
+        var preppedCapsule: some View {
+            shape
+                .fill(viewModel.preppedColor.gradient)
+                .frame(width: preppedWidth(for: proxy))
+        }
+        
+        var eatenCapsule: some View {
+            shape
+                .fill(viewModel.eatenColor.gradient)
+                .frame(width: eatenWidth(proxy: proxy))
+        }
+
+        var incrementCapsule: some View {
+            shape
+                .fill(viewModel.incrementColor.gradient)
+                .frame(width: incrementWidth(proxy: proxy))
+        }
+        
+        @ViewBuilder
+        var lowerGoalMark: some View {
+            if viewModel.shouldShowLowerGoalMark {
+                DottedLine()
+                    .stroke(style: StrokeStyle(
+                        lineWidth: viewModel.lowerGoalMarkLineWidth,
+                        dash: [viewModel.lowerGoalMarkDash])
+                    )
+                    .frame(width: 1)
+                    .foregroundColor(viewModel.lowerGoalMarkColor)
+                    .opacity(viewModel.lowerGoalMarkOpacity)
+                    .offset(x: lowerGoalMarkOffset(for: proxy))
+            }
+        }
+
+        @ViewBuilder
+        var upperGoalMark: some View {
+            if viewModel.shouldShowUpperGoalMark {
+                DottedLine()
+                    .stroke(style: StrokeStyle(
+                        lineWidth: viewModel.upperGoalMarkLineWidth,
+                        dash: [viewModel.upperGoalMarkDash])
+                    )
+                    .frame(width: 1)
+                    .foregroundColor(viewModel.upperGoalMarkColor)
+                    .opacity(viewModel.upperGoalMarkOpacity)
+                    .offset(x: upperGoalMarkOffset(for: proxy))
+            }
+        }
+
+        return ZStack(alignment: .leading) {
+            placeholderCapsule
+            incrementCapsule
+            preppedCapsule
+            eatenCapsule
+            lowerGoalMark
+            upperGoalMark
+        }
+    }
+    
+    //MARK: - Accessors
+    var food: Double {
+        viewModel.planned
+    }
+    
+    //MARK: - 📐 Widths
+    func preppedWidth(for proxy: GeometryProxy) -> Double {
+        proxy.size.width * viewModel.preppedPercentageForMeter
+    }
+    
+    func eatenWidth(proxy: GeometryProxy) -> Double {
+        proxy.size.width * viewModel.eatenPercentage
+    }
+    
+    func incrementWidth(proxy: GeometryProxy) -> Double {
+        proxy.size.width * viewModel.incrementPercentageForMeter
+    }
+    
+    func lowerGoalMarkOffset(for proxy: GeometryProxy) -> Double {
+        (proxy.size.width * viewModel.lowerGoalPercentage) - 1.0
+    }
+
+    func upperGoalMarkOffset(for proxy: GeometryProxy) -> Double {
+        (proxy.size.width * viewModel.upperGoalPercentage) - 1.0
+    }
+
+    //MARK: - 🎨 Colors
+    var placeholderColor: Color {
+        Colors.placeholder
+    }
+
+    //MARK: - 🎞 Animations
+    var animation: Animation {
+        .default
+        //TODO: Rewrite this
+//        if eaten <= 0.05 || eaten >= 0.95 {
+//            /// don't bounce the bar if we're going to 0 or going close to 1
+//            return .interactiveSpring()
+//        } else {
+//            return .interactiveSpring(response: 0.35, dampingFraction: 0.66, blendDuration: 0.35)
+//        }
+    }
+    
+    //MARK: - Enums
+    struct Colors {
+        static let placeholder = Color("StatsEmptyFill", bundle: .module)
+    }
+}
+
+extension NutrientMeter2.ViewModel {
+    
+    var shouldShowLowerGoalMark: Bool {
+        switch goalBoundsType {
+            
+        case .lowerAndUpper:
+            /// Always shows
+            return true
+            
+        case .lowerOnly:
+            /// Shows if the endPoint exceeds the lower mark
+            guard let goalLower else { return false }
+            return endPoint > goalLower
+            
+        default:
+            return false
+        }
+    }
+    
+    var shouldShowUpperGoalMark: Bool {
+        switch goalBoundsType {
+            
+        case .lowerAndUpper, .upperOnly:
+            /// Shows if the endPoint exceeds the upper mark
+            guard let goalUpper else { return false }
+            return endPoint > goalUpper
+            
+        default:
+            return false
+        }
+    }
+    var preppedPercentageType: PercentageType {
+        let defaultType = PercentageType(preppedPercentage)
+        switch goalBoundsType {
+        case .none, .lowerOnly:
+            return preppedPercentage < 1 ? defaultType : .complete
+        case .upperOnly:
+            return preppedPercentage < 1 ? .complete : .excess
+        case .lowerAndUpper:
+            guard let goalLower, let goalUpper else { return defaultType }
+            if planned < goalLower {
+                return defaultType
+            } else if planned < goalUpper {
+                return .complete
+            } else {
+                return .excess
+            }
+        }
+    }
+    
+    var incrementPercentageType: PercentageType {
+        guard let increment else { return .regular }
+        let defaultType = PercentageType(incrementPercentage)
+        switch goalBoundsType {
+        case .none, .lowerOnly:
+            return incrementPercentage < 1 ? defaultType : .complete
+        case .upperOnly:
+            return incrementPercentage < 1 ? .complete : .excess
+        case .lowerAndUpper:
+            guard let goalLower, let goalUpper else { return defaultType }
+            if planned + increment < goalLower {
+                return defaultType
+            } else if planned + increment < goalUpper {
+                return .complete
+            } else {
+                return .excess
+            }
+        }
+    }
+
+    var incrementPercentage: Double {
+        guard let increment = increment else { return 0 }
+        guard totalGoal != 0 else {
+            return 1
+        }
+        return (increment + planned) / totalGoal
+    }
+    
+    var incrementPercentageForMeter: Double {
+        guard let increment = increment, increment > 0 else { return 0 }
+        //        guard let increment = increment?.wrappedValue, totalGoal != 0 else { return 0 }
+        
+        guard totalGoal != 0 else {
+            return 1
+        }
+        
+        /// Choose greater of goal or "prepped + increment"
+        let total: Double
+        if planned + increment > totalGoal {
+            total = planned + increment
+        } else {
+            total = totalGoal
+        }
+        
+        return ((increment / total) + preppedPercentage)
+    }
+    
+    
+    var endPoint: Double {
+        if let increment {
+            return planned + increment
+        } else {
+            return planned
+        }
+    }
+    
+    //MARK: Lower Goal
+    var lowerGoalPercentage: CGFloat {
+        guard let goalLower else { return 0 }
+        //TODO: Possibly remove this redundant conditional by always using endPoint instead
+        if let goalUpper, goalUpper > 0 {
+            return goalLower / max(goalUpper, endPoint)
+        } else {
+            guard endPoint > 0 else { return 0}
+            return goalLower / endPoint
+        }
+    }
+    var lowerGoalMarkColor: Color {
+        Color(.systemGroupedBackground)
+//        Color.white
+//        Color(.label)
+//        let percentageType = showingIncrement ? incrementPercentageType : preppedPercentageType
+//        switch percentageType {
+//        case .excess:
+//            return Colors.Excess.text
+//        default:
+//            return Colors.Complete.text
+//        }
+    }
+    
+    var lowerGoalMarkOpacity: CGFloat {
+        lowerGoalMarkOverBar ? 0.5 : 1.0
+    }
+    
+    var lowerGoalMarkLineWidth: CGFloat {
+        lowerGoalMarkOverBar ? 1 : 1.5
+    }
+    
+    var lowerGoalMarkOverBar: Bool {
+        guard let goalLower else { return false }
+        return goalLower < endPoint
+    }
+    
+    var lowerGoalMarkDash: CGFloat {
+        lowerGoalMarkOverBar ? 2 : 100
+    }
+    
+    //MARK: Upper Goal
+    
+    var upperGoalPercentage: CGFloat {
+        guard let goalUpper, endPoint > 0 else { return 0 }
+        return goalUpper / endPoint
+    }
+    var upperGoalMarkColor: Color {
+        Color(.systemGroupedBackground)
+//        Color.white
+//        Color(.label)
+//        let percentageType = showingIncrement ? incrementPercentageType : preppedPercentageType
+//        switch percentageType {
+//        case .excess:
+//            return Colors.Excess.text
+//        default:
+//            return Colors.Complete.text
+//        }
+    }
+    
+    var upperGoalMarkOpacity: CGFloat {
+        upperGoalMarkOverBar ? 0.5 : 1.0
+    }
+    
+    var upperGoalMarkLineWidth: CGFloat {
+        upperGoalMarkOverBar ? 1 : 1.5
+    }
+    
+    var upperGoalMarkOverBar: Bool {
+        guard let goalUpper else { return false }
+        return goalUpper < endPoint
+    }
+    
+    var upperGoalMarkDash: CGFloat {
+        upperGoalMarkOverBar ? 2 : 100
+    }
+}
