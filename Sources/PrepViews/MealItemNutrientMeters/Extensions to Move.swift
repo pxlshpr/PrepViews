32b0a4f572afb761df9fd6068cd8aea3d8b310f4
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-29 00:16:23 +0500

    tag increment

--- Sources/PrepViews/MealItemNutrientMeters/Extensions to Move.swift
+++ Sources/PrepViews/MealItemNutrientMeters/Extensions to Move.swift
@@ -20,6 +20,7 @@ extension Day {
         meals.filter { !$0.isPlannedOrWithType }
     }
     
+    
     func existingAmount(for component: NutrientMeterComponent, lowerBound: Bool, params: GoalCalcParams) -> Double {
         meals.reduce(0) { partialResult, meal in
             let value = meal.plannedOrTypeValue(for: component, lowerBound: lowerBound, params: params) ?? 0
