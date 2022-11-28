0e5e250ef099496469a5e404736a2d01324e47f9
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-28 22:02:49 +0500

    tag increment

--- Sources/PrepViews/NutrientMeter/PercentageType.swift
+++ Sources/PrepViews/NutrientMeter/PercentageType.swift
@@ -8,19 +8,33 @@ public enum PercentageType {
 
 public extension PercentageType {
     init(_ value: Double) {
-        let CompletionCutOffPercentage: Double = 5
-        let cutoff = CompletionCutOffPercentage/100.0
-        let lowerCompletionBound = 1 - cutoff
-        let upperCompletionBound = 1 + cutoff
-
         if value == 0 {
             self = .empty
-        } else if value >= lowerCompletionBound && value <= upperCompletionBound {
+        } else if value == 1.0 {
             self = .complete
-        } else if value > upperCompletionBound {
+        } else if value > 1.0 {
             self = .excess
         } else {
             self = .regular
         }
     }
+    
+    /// Legacy init that was used with a cutoff to allow for completions to be registered close to the end (above or below it)
+    /// We stopped using this after employing the new bound-based goals
+//    init(_ value: Double) {
+//        let CompletionCutOffPercentage: Double = 5
+//        let cutoff = CompletionCutOffPercentage/100.0
+//        let lowerCompletionBound = 1 - cutoff
+//        let upperCompletionBound = 1 + cutoff
+//
+//        if value == 0 {
+//            self = .empty
+//        } else if value >= lowerCompletionBound && value <= upperCompletionBound {
+//            self = .complete
+//        } else if value > upperCompletionBound {
+//            self = .excess
+//        } else {
+//            self = .regular
+//        }
+//    }
 }
