839e0ce79ee5fb008d969d8a486f686da35d0dc9
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-13 07:55:45 +0500

    bump

--- Sources/PrepViews/NutrientMeter/NutrientMeter.swift
+++ Sources/PrepViews/NutrientMeter/NutrientMeter.swift
@@ -43,9 +43,16 @@ public extension NutrientMeter {
         }
         
         var preppedCapsule: some View {
-            shape
+            
+            var fill: some ShapeStyle {
+                viewModel.increment == nil
+                ? viewModel.eatenColor.gradient
+                : viewModel.preppedColor.gradient
+            }
+            
+            return shape
 //                .fill(viewModel.preppedColor.gradient)
-                .fill(viewModel.eatenColor.gradient)
+                .fill(fill)
                 .frame(width: preppedWidth(for: proxy))
                 .if(hasAppeared, transform: { view in
                     /// Use this to make sure the animation only gets applied after the view has appeared, so that it doesn't animate it during it being transitioned into view
